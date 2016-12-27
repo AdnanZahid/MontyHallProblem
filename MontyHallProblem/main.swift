11982aca202683a2cf7432c828be687bce58a079
Adnan Zahid <adnan.zahid@vistajet.com>
2016-12-27 14:54:50 +0500

    Initial Commit

---
+++ MontyHallProblem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MontyHallProblem
+//
+//  Created by Adnan Zahid on 12/27/16.
+//  Copyright © 2016 Adnan Zahid. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
