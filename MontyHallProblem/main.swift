ebfe2d688b10e86a44467c344ece4af0d9d8dec3
Adnan Zahid <adnan.zahid@vistajet.com>
2016-12-27 15:54:12 +0500

    Monty hall problem proved
    Monty hall problem proved

--- MontyHallProblem/main.swift
+++ MontyHallProblem/main.swift
@@ -8,5 +8,51 @@
 
 import Foundation
 
-print("Hello, World!")
+func openDoorIfNotChosen(chosen: UInt32, car: UInt32) -> UInt32 {
+    
+    let toBeOpened = arc4random_uniform(3)
+    
+    if toBeOpened == chosen || toBeOpened == car {
+        
+        return openDoorIfNotChosen(chosen: chosen, car: car)
+        
+    } else {
+        
+        return toBeOpened
+    }
+}
 
+func montyHallProblemSuccess() -> Bool {
+    
+    let car = arc4random_uniform(3)
+    
+    let chosen = arc4random_uniform(3)
+    
+    let opened = openDoorIfNotChosen(chosen: chosen, car: car)
+    
+    var notChosen: UInt32 = 0
+    
+    for i: UInt32 in 0 ... 2 {
+        
+        if i != chosen && i != opened {
+            
+            notChosen = i
+        }
+    }
+    
+    return notChosen == car
+}
+
+var success = 0
+
+var maxTrials = 1000000
+
+for i in 0 ... maxTrials {
+    
+    if montyHallProblemSuccess() == true {
+        
+        success = success + 1
+    }
+}
+
+print(success)
